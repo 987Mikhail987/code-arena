ae5cb76ee25ad862073f1a07e88844c02f62be34
987Mikhail987 <misha--kondratev@yandex.com>
2026-05-14 16:26:02 +0300

    refactor:server

--- server/src/routes/authRoute.js
+++ server/src/routes/authRoute.js
@@ -5,7 +5,6 @@ const verifyRefreshToken = require("../middleware/verifyRefreshToken");
 authRouter.post("/register", AuthController.register);
 authRouter.post("/login", AuthController.login);
 authRouter.post("/logout", AuthController.logout);
-authRouter.get("/refresh", verifyRefreshToken, AuthController.refreshTokens);
 authRouter.post("/refresh", verifyRefreshToken, AuthController.refreshTokens);
 
 module.exports = authRouter;
