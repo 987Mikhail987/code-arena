9bc7095b4243d49367d6602dace3abaa89564189
987Mikhail987 <misha--kondratev@yandex.com>
2026-05-15 16:09:31 +0300

    merge:room-Edit-Redactor

--- client/src/app/room/[id]/page.tsx
+++ client/src/app/room/[id]/page.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import Chat from "@/widgets/ChatwithAi/ChatwithAi";
+import Redactor from "@/widgets/Redactor/Redactor";
+import React from "react";
 
-export default function page() {
+export default function RoomPage() {
   return (
-    <div>Комната для собеседований</div>
-  )
+    <div>
+      <h2>Добро пожаловать на собеседование</h2>
+      <Chat />
+      <Redactor />
+    </div>
+  );
 }
