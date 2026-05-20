747da01c5ce108d94aee28322e0a60ba19b4d695
987Mikhail987 <misha--kondratev@yandex.com>
2026-05-20 16:42:21 +0300

    fix-build

--- client/src/shared/lib/liveSocket.ts
+++ client/src/shared/lib/liveSocket.ts
@@ -1,4 +1,4 @@
-import { io } from "socket.io-client";
+import io from "socket.io-client";
 import { getAccessToken } from "./axiosInstance";
 
 export type LiveSocket = ReturnType<typeof createLiveSocket>;
@@ -6,7 +6,6 @@ export type LiveSocket = ReturnType<typeof createLiveSocket>;
 export function createLiveSocket() {
   return io(process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000", {
     autoConnect: false,
-    withCredentials: true,
     auth: {
       accessToken: getAccessToken(),
     },
