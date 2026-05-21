269da96bf430de9020bffa5ed84c5dcf7471dfbc
987Mikhail987 <misha--kondratev@yandex.com>
2026-05-21 11:22:22 +0300

    fix: sessionService

--- server/src/services/SessionService.js
+++ server/src/services/SessionService.js
@@ -181,48 +181,48 @@ class SessionService {
     return null;
   }
 
-  static async getSessionByIdentifier(identifier, user) {
-    const isNumericId = !Number.isNaN(Number(identifier));
-    const sessionWhere = isNumericId
-      ? { id: Number(identifier) }
-      : { public_id: identifier, type: "live" };
+  // static async getSessionByIdentifier(identifier, user) {
+  //   const isNumericId = !Number.isNaN(Number(identifier));
+  //   const sessionWhere = isNumericId
+  //     ? { id: Number(identifier) }
+  //     : { public_id: identifier, type: "live" };
 
-    const accessWhere =
-      user.role === "intervier"
-        ? {
-            ...sessionWhere,
-            type: "live",
-            status: "active",
-          }
-        : {
-            ...sessionWhere,
-            [Op.or]: [{ user_id: user.id }, { public_id: identifier }],
-          };
+  //   const accessWhere =
+  //     user.role === "intervier"
+  //       ? {
+  //           ...sessionWhere,
+  //           type: "live",
+  //           status: "active",
+  //         }
+  //       : {
+  //           ...sessionWhere,
+  //           [Op.or]: [{ user_id: user.id }, { public_id: identifier }],
+  //         };
 
-    return Session.findOne({
-      where: accessWhere,
-      include: [
-        {
-          model: User,
-          as: "user",
-          attributes: ["id", "name", "email", "role"],
-          ...(user.role === "intervier"
-            ? {
-                where: {
-                  role: "candidate",
-                },
-              }
-            : {}),
-        },
-        {
-          model: Message,
-          as: "messages",
-          separate: true,
-          order: [["createdAt", "ASC"]],
-        },
-      ],
-    });
-  }
+  //   return Session.findOne({
+  //     where: accessWhere,
+  //     include: [
+  //       {
+  //         model: User,
+  //         as: "user",
+  //         attributes: ["id", "name", "email", "role"],
+  //         ...(user.role === "intervier"
+  //           ? {
+  //               where: {
+  //                 role: "candidate",
+  //               },
+  //             }
+  //           : {}),
+  //       },
+  //       {
+  //         model: Message,
+  //         as: "messages",
+  //         separate: true,
+  //         order: [["createdAt", "ASC"]],
+  //       },
+  //     ],
+  //   });
+  // }
 
   static async getUserSessionById(sessionId, userId) {
     return Session.findOne({
